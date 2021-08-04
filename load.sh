a129823af786eb44bced8a3984d8720a41bfc76c
Chris Zubak-Skees <chriszs@gmail.com>
2021-08-04 19:14:31 -0400

    Refactor to publish datasette

--- load.sh
+++ load.sh
@@ -5,10 +5,10 @@ set -euo pipefail
 wget -nv https://forms.irs.gov/app/pod/dataDownload/fullData
 unzip fullData
 python process_data.py
-psql -f create.sql
-psql -c "COPY irs527_filings FROM STDIN WITH CSV DELIMITER '|' QUOTE E'\b'" < "line_2.txt"
-psql -c "COPY irs527_contributions FROM STDIN WITH CSV DELIMITER '|' QUOTE E'\b'" < "line_A.txt"
-psql -c "COPY irs527_expenditures FROM STDIN WITH CSV DELIMITER '|' QUOTE E'\b'" < "line_B.txt"
+csvs-to-sqlite line_2.txt 527s.db --separator '|' --quoting 3 --table=filings
+csvs-to-sqlite line_A.txt 527s.db --separator '|' --quoting 3 --table=contributions
+csvs-to-sqlite line_B.txt 527s.db --separator '|' --quoting 3 --table=expenditures
 rm -rf var
 rm *.txt
 rm fullData
+rm 527s.db
