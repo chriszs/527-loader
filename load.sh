f23c8e5b8d6ea42819008dd2185e8810397fcac4
Chris Zubak-Skees <chriszs@gmail.com>
2021-08-04 20:06:40 -0400

    Update load.sh

--- load.sh
+++ load.sh
@@ -6,9 +6,9 @@ wget -nv https://forms.irs.gov/app/pod/dataDownload/fullData
 unzip fullData
 python process_data.py
 mkdir out
-cat headers/line_2.txt filings.txt > out/filings.txt
-cat headers/line_A.txt contributions.txt > out/contributions.txt
-cat headers/line_B.txt expenditures.txt > out/expenditures.txt
+cat headers/filings.txt line_2.txt > out/filings.txt
+cat headers/contributions.txt line_A.txt > out/contributions.txt
+cat headers/expenditures.txt line_B.txt > out/expenditures.txt
 csvs-to-sqlite filings.txt 527s.db --separator '|' --quoting 3 --just-strings
 csvs-to-sqlite contributions.txt 527s.db --separator '|' --quoting 3 --shape contribution_amount:contribution_amount(INTEGER) --date contribution_date --just-strings
 csvs-to-sqlite expenditures.txt 527s.db --separator '|' --quoting 3 --shape expenditure_amount:expenditure_amount(INTEGER) --date expenditure_date --just-strings
