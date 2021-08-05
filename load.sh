a9cddba63a769648d832275bb221a779c52a5ee5
Chris Zubak-Skees <chriszs@gmail.com>
2021-08-04 22:00:56 -0400

    Update load.sh

--- load.sh
+++ load.sh
@@ -10,8 +10,8 @@ cat headers/filings.txt line_2.txt > out/filings.txt
 cat headers/contributions.txt line_A.txt > out/contributions.txt
 cat headers/expenditures.txt line_B.txt > out/expenditures.txt
 csvs-to-sqlite out/filings.txt 527s.db --separator '|' --quoting 3 --just-strings
-csvs-to-sqlite out/contributions.txt 527s.db --separator '|' --quoting 3 --shape 'contribution_amount:contribution_amount\(integer\),contribution_date:contribution_date\(date\)' --just-strings
-csvs-to-sqlite out/expenditures.txt 527s.db --separator '|' --quoting 3 --shape 'expenditure_amount:expenditure_amount\(integer\),expenditure_date:expenditure_date\(date\)' --just-strings
+csvs-to-sqlite out/contributions.txt 527s.db --separator '|' --quoting 3 --shape
+csvs-to-sqlite out/expenditures.txt 527s.db --separator '|' --quoting 3 --shape
 rm -rf var
 rm *.txt
 rm fullData
