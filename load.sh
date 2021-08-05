dc3fdf09a59d98d42ed3e7bd78fba0d011c8aaf6
Chris Zubak-Skees <chriszs@gmail.com>
2021-08-04 22:46:07 -0400

    Fix fts param issue

--- load.sh
+++ load.sh
@@ -10,8 +10,8 @@ cat headers/filings.txt line_2.txt > out/filings.txt
 cat headers/contributions.txt line_A.txt > out/contributions.txt
 cat headers/expenditures.txt line_B.txt > out/expenditures.txt
 csvs-to-sqlite out/filings.txt 527s.db --separator '|' --quoting 3 --just-strings
-csvs-to-sqlite out/contributions.txt 527s.db --separator '|' --quoting 3 --fts org_name -fts ein
-csvs-to-sqlite out/expenditures.txt 527s.db --separator '|' --quoting 3 --fts org_name -fts ein
+csvs-to-sqlite out/contributions.txt 527s.db --separator '|' --quoting 3 --fts org_name ein
+csvs-to-sqlite out/expenditures.txt 527s.db --separator '|' --quoting 3 --fts org_name ein
 rm -rf var
 rm *.txt
 rm fullData
