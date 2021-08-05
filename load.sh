31f272baee1b838b96cd2a896f5c098cc549ed1b
Chris Zubak-Skees <chriszs@gmail.com>
2021-08-04 20:54:10 -0400

    Update

--- load.sh
+++ load.sh
@@ -2,16 +2,16 @@
 
 set -euo pipefail
 
-#wget -nv https://forms.irs.gov/app/pod/dataDownload/fullData
-#unzip fullData
-#python process_data.py
-#mkdir out
-#cat headers/filings.txt line_2.txt > out/filings.txt
-#cat headers/contributions.txt line_A.txt > out/contributions.txt
-#cat headers/expenditures.txt line_B.txt > out/expenditures.txt
+wget -nv https://forms.irs.gov/app/pod/dataDownload/fullData
+unzip fullData
+python process_data.py
+mkdir out
+cat headers/filings.txt line_2.txt > out/filings.txt
+cat headers/contributions.txt line_A.txt > out/contributions.txt
+cat headers/expenditures.txt line_B.txt > out/expenditures.txt
 csvs-to-sqlite out/filings.txt 527s.db --separator '|' --quoting 3 --just-strings
-csvs-to-sqlite out/contributions.txt 527s.db --separator '|' --quoting 3 --shape 'contribution_amount:contribution_amount\\\(integer\\\),contribution_date:contribution_date\\\(date\\\)' --just-strings
-csvs-to-sqlite out/expenditures.txt 527s.db --separator '|' --quoting 3 --shape 'expenditure_amount:expenditure_amount\\\(integer\\\),expenditure_date:expenditure_date\\\(date\\\)' --just-strings
+csvs-to-sqlite out/contributions.txt 527s.db --separator '|' --quoting 3 --shape 'contribution_amount:contribution_amount\(integer\),contribution_date:contribution_date\(date\)' --just-strings
+csvs-to-sqlite out/expenditures.txt 527s.db --separator '|' --quoting 3 --shape 'expenditure_amount:expenditure_amount\(integer\),expenditure_date:expenditure_date\(date\)' --just-strings
 rm -rf var
 rm *.txt
 rm fullData
